f74cf999db150b3f18c6ce036e3bc98dd2761d19
Diego Palma <dpalmasan@fb.com>
2024-06-26 17:41:46 -0700

    Add board generation function

--- n2_puzzle/puzzle.py
+++ n2_puzzle/puzzle.py
@@ -9,6 +9,58 @@ class Direction(Enum):
     RIGHT = 3
 
 
+def generate_template_board(n, is_goal: bool = False):
+    """Generate a board with the worst case given a dimension n.
+
+    In this case we generate a board with the reversed ordering of
+    the tiles. The only catch is that for the puzzle to be solvable
+    the last two tiles should be swapped if n is even.
+
+    For example, for n = 4, the board is:
+
+    -----------------
+    | 15| 14| 13| 12|
+    -----------------
+    | 11| 10|  9|  8|
+    -----------------
+    |  7|  6|  5|  4|
+    -----------------
+    |  3|  1|  2|   |
+    -----------------
+
+    Note that for n = 3 the last two tiles are not swapped:
+
+    -------------
+    |  8|  7|  6|
+    -------------
+    |  5|  4|  3|
+    -------------
+    |  2|  1|   |
+    -------------
+
+    If `is_goal` is True, the board is the goal state of the puzzle.
+    """
+    board = []
+    curr = n * n - 1 if not is_goal else 1
+    for _ in range(n):
+        row = []
+        for _ in range(n):
+            row.append(curr)
+            curr -= 1 if not is_goal else -1
+        board.append(row)
+    board[n - 1][n - 1] = 0
+
+    # For the puzzle to be solvable in the case of
+    # even number of tiles the last two tiles should
+    # be swapped
+    if n % 2 == 0 and not is_goal:
+        board[n - 1][n - 2], board[n - 1][n - 3] = (
+            board[n - 1][n - 3],
+            board[n - 1][n - 2],
+        )
+    return board
+
+
 class NPuzzle:
     def __init__(self, board: List[List[int]], is_goal: bool = False) -> None:
         # Simple assertions to make sure the board is "valid"
